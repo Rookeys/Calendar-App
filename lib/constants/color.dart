54f961ce6322b354f9f0d84b2d20249f4d65ea4c
KoJaem <woalswhwh@naver.com>
2023-09-13 13:47:29 +0900

    feat : Color setting

---
+++ lib/constants/color.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static const Color skyBlue = Color(0x28C2F2FF);
+  static const Color pastelBlue = Color(0x99DBF5FF);
+  static const Color lightPurple = Color(0xBBCEFFFF);
+  static const Color lightYellow = Color(0xFFEEBBFF);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color darkGray = Color(0x777777FF);
+  static const Color red = Color(0xFF5942FF);
+  static const Color green = Color(0x5BAD68FF);
+  static const Color thickPink = Color(0xD8A6A6FF);
+}
