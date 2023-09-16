43f55138a012670795b91450a8032b03906382fb
KoJaem <woalswhwh@naver.com>
2023-09-16 17:58:30 +0900

    fix : Modified customColor

---
+++ lib/constants/customColor.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static const Color skyBlue = Color(0xFF28C2F2);
+  static const Color pastelBlue = Color(0xFF99DBF5);
+  static const Color lightPurple = Color(0xFFBBCEFF);
+  static const Color lightYellow = Color(0xFFFFEEBB);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color darkGray = Color(0xFF777777);
+  static const Color red = Color(0xFFFF5942);
+  static const Color green = Color(0xFF5BAD68);
+  static const Color thickPink = Color(0xFFD8A6A6);
+}
