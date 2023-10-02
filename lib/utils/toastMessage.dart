7150bc960dae46d95c5002ecd1efec06c95d01ba
KoJaem <woalswhwh@naver.com>
2023-10-02 22:40:55 +0900

    feat : toastMessage 구현 #3 (flutter toast 라이브러리 사용)

---
+++ lib/utils/toastMessage.dart
@@ -0,0 +1,68 @@
+import 'package:calendar_app/constants/customColor.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void showCustomMessage(
+  String message, {
+  ToastGravity position = ToastGravity.BOTTOM,
+  double fontSize = 18,
+  Color bgColor = CustomColor.darkGray,
+  Color textColor = CustomColor.white,
+}) {
+  Fluttertoast.showToast(
+    msg: message,
+    toastLength: Toast.LENGTH_LONG,
+    fontSize: fontSize,
+    gravity: position,
+    backgroundColor: bgColor,
+    textColor: textColor,
+  );
+}
+
+void showSuccessMessage(
+  String message, {
+  ToastGravity position = ToastGravity.BOTTOM,
+  double fontSize = 18,
+  Color bgColor = CustomColor.green,
+  Color textColor = CustomColor.white,
+}) {
+  showCustomMessage(
+    message,
+    position: position,
+    fontSize: fontSize,
+    bgColor: bgColor,
+    textColor: textColor,
+  );
+}
+
+void showWarningMessage(
+  String message, {
+  ToastGravity position = ToastGravity.BOTTOM,
+  double fontSize = 18,
+  Color bgColor = CustomColor.orange,
+  Color textColor = CustomColor.white,
+}) {
+  showCustomMessage(
+    message,
+    position: position,
+    fontSize: fontSize,
+    bgColor: bgColor,
+    textColor: textColor,
+  );
+}
+
+void showErrorMessage(
+  String message, {
+  ToastGravity position = ToastGravity.BOTTOM,
+  double fontSize = 18,
+  Color bgColor = CustomColor.red,
+  Color textColor = CustomColor.white,
+}) {
+  showCustomMessage(
+    message,
+    position: position,
+    fontSize: fontSize,
+    bgColor: bgColor,
+    textColor: textColor,
+  );
+}
