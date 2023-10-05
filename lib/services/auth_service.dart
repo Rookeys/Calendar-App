a157197cfd4816f7870f016087096477d6aa5ea0
froggy1014 <clcl6084@gmail.com>
2023-10-06 02:27:04 +0900

    feat: refactor googleSignIn exception logic #2

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -2,59 +2,61 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+FirebaseAuthException handleAuthException(FirebaseAuthException e) {
+  if (e.code == 'user-cancelled') {
+    return FirebaseAuthException(
+      code: 'user-cancelled',
+      message: 'User cancelled the sign-in flow',
+    );
+  } else if (e.code == 'invalid-email-domain') {
+    return FirebaseAuthException(
+      code: 'invalid-email-domain',
+      message:
+          'Only users with email addresses from [icloudhospital.com] are allowed to sign in',
+    );
+  } else {
+    return FirebaseAuthException(
+      code: 'login-failed',
+      message: 'Login failed: ${e.message}',
+    );
+  }
+}
+
 class AuthService {
-  // google sign in
   Future<UserCredential> signInWithGoogle(BuildContext context) async {
     try {
       final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
 
       if (gUser == null) {
-        // User cancelled the sign-in flow
         throw FirebaseAuthException(
           code: 'user-cancelled',
           message: 'User cancelled the sign-in flow',
         );
       }
 
-      // Check if the user's email domain is allowed
       final allowedDomains = ['icloudhospital.com'];
       final emailDomain = gUser.email.split('@').last;
       if (!allowedDomains.contains(emailDomain)) {
-        // User's email domain is not allowed
         throw FirebaseAuthException(
           code: 'invalid-email-domain',
-          message:
-              'Only users with email addresses from $allowedDomains are allowed to sign in',
         );
       }
 
-      // create auth details from request
       final GoogleSignInAuthentication gAuth = await gUser.authentication;
 
-      // create a new credential for use
       final credential = GoogleAuthProvider.credential(
         accessToken: gAuth.accessToken,
         idToken: gAuth.idToken,
       );
 
-      // finally lets sign in
       return await FirebaseAuth.instance.signInWithCredential(credential);
     } on FirebaseAuthException catch (e) {
-      // Handle FirebaseAuthException
-      if (e.code == 'user-cancelled') {
-        // User cancelled the sign-in flow
-        print('User cancelled the sign-in flow');
-        // throw 'User cancelled the sign-in flow';
-      } else {
-        // Other FirebaseAuthException
-        print('FirebaseAuth`Exception: ${e.message}');
-        // throw 'FirebaseAuth`Exception: ${e.message}';
-      }
-      rethrow;
+      throw handleAuthException(e);
     } catch (e) {
-      // Handle other exceptions
-      print('Exception: $e');
-      throw 'Exception: error signing in with google';
+      throw FirebaseAuthException(
+        code: 'login-failed',
+        message: 'Login failed',
+      );
     }
   }
 }
