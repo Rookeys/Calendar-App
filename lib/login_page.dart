21c9333c32905661da317f679e71df21a63374fc
froggy1014 <clcl6084@gmail.com>
2023-10-06 00:49:56 +0900

    feat: enhance error Handling #2

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:calendar_app/components/error_popup.dart';
 import 'package:calendar_app/constants/custom_color.dart';
 import 'package:calendar_app/services/auth_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -17,7 +18,6 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   // text editing controllers
   final emailController = TextEditingController();
-
   final passwordController = TextEditingController();
 
   @override
@@ -25,119 +25,145 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       backgroundColor: CustomColor.skyBlue,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25),
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            // logo
-            const Icon(Icons.verified_user_rounded, size: 100),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // logo
+                      const Icon(Icons.verified_user_rounded, size: 100),
 
-            const SizedBox(height: 50),
+                      const SizedBox(height: 50),
 
-            // email textfield
-            FormTextField(
-              controller: emailController,
-              hintText: 'E-mail',
-              obscureText: false,
-            ),
+                      // email textfield
+                      FormTextField(
+                        controller: emailController,
+                        hintText: 'E-mail',
+                        obscureText: false,
+                      ),
 
-            const SizedBox(height: 25),
+                      const SizedBox(height: 25),
 
-            // password textfield
-            FormTextField(
-              controller: passwordController,
-              hintText: 'Password',
-              obscureText: true,
-            ),
+                      // password textfield
+                      FormTextField(
+                        controller: passwordController,
+                        hintText: 'Password',
+                        obscureText: true,
+                      ),
 
-            const SizedBox(height: 50),
+                      const SizedBox(height: 50),
 
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  LinkText(
-                    icon: const Icon(
-                      Icons.check_circle_outline,
-                      color: CustomColor.white,
-                    ),
-                    text: const Text(
-                      'Auto login',
-                      style: TextStyle(
-                          color: CustomColor.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16),
-                    ),
-                    onTap: () {
-                      // print('auto login');
-                    },
-                  ),
-                  const SizedBox(height: 20), // add a gap of 10 pixels
-                  LinkText(
-                    icon: const Icon(
-                      Icons.settings,
-                      color: CustomColor.white,
-                    ),
-                    text: const Text(
-                      'Forgot password?',
-                      style: TextStyle(
-                          color: CustomColor.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                          decoration: TextDecoration.underline,
-                          decorationColor: CustomColor.white),
-                    ),
-                    onTap: () {
-                      // print('forgot password');
-                    },
-                  ),
-                ],
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 25),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            LinkText(
+                              icon: const Icon(
+                                Icons.check_circle_outline,
+                                color: CustomColor.white,
+                              ),
+                              text: const Text(
+                                'Auto login',
+                                style: TextStyle(
+                                    color: CustomColor.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16),
+                              ),
+                              onTap: () {
+                                // print('auto login');
+                              },
+                            ),
+                            const SizedBox(
+                                height: 20), // add a gap of 10 pixels
+                            LinkText(
+                              icon: const Icon(
+                                Icons.settings,
+                                color: CustomColor.white,
+                              ),
+                              text: const Text(
+                                'Forgot password?',
+                                style: TextStyle(
+                                    color: CustomColor.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                    decoration: TextDecoration.underline,
+                                    decorationColor: CustomColor.white),
+                              ),
+                              onTap: () {
+                                // print('forgot password');
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+
+                      const SizedBox(height: 25),
+
+                      // sign in button
+                      LoginButton(
+                        onTap: () {
+                          signUserIn();
+                        },
+                        text: const Text(
+                          'Login',
+                          style: TextStyle(
+                            color: CustomColor.skyBlue,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                          ),
+                        ),
+                        backgroundColor: CustomColor.white,
+                      ),
+
+                      const SizedBox(height: 15),
+
+                      LoginButton(
+                        onTap: () {
+                          signUserInWithGoogle();
+                        },
+                        text: const Text(
+                          'Sign up with Google',
+                          style: TextStyle(
+                            color: CustomColor.darkGray,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        image: Image.asset('assets/images/google.png',
+                            width: 24, height: 24, fit: BoxFit.cover),
+                        backgroundColor: CustomColor.white,
+                      ),
+
+                      //number? regoster now
+                    ]),
               ),
             ),
-
-            const SizedBox(height: 25),
-
-            // sign in button
-            LoginButton(
-              onTap: () {
-                signUserIn();
-              },
-              text: const Text(
-                'Login',
-                style: TextStyle(
-                  color: CustomColor.skyBlue,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24,
-                ),
-              ),
-              backgroundColor: CustomColor.white,
-            ),
-
-            const SizedBox(height: 15),
-
-            LoginButton(
-              onTap: () {
-                // print('google sign in');
-                AuthService().signInWithGoogle();
-              },
-              text: const Text(
-                'Sign up with Google',
-                style: TextStyle(
-                  color: CustomColor.darkGray,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                ),
-              ),
-              image: Image.asset('assets/images/google.png',
-                  width: 24, height: 24, fit: BoxFit.cover),
-              backgroundColor: CustomColor.white,
-            ),
-
-            //number? regoster now
-          ]),
+          ],
         ),
       ),
     );
+  }
+
+  void displayErrorMessage(String message, String s) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Error Message'),
+            content: Text(message),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('Close'),
+              ),
+            ],
+          );
+        });
   }
 
   // sign in function
@@ -155,32 +181,28 @@ class _LoginPageState extends State<LoginPage> {
       Navigator.pop(context);
     } on FirebaseAuthException catch (e) {
       Navigator.pop(context);
-      if (e.code == 'user-not-found') {
-        wrongEmailMessage();
-      } else if (e.code == 'wrong-password') {
-        wrongPasswordMessage();
-      }
+      showErrorDialog(context, e.code);
+    } catch (e) {
+      showErrorDialog(context, 'please check your email and password');
     }
   }
 
-  // wrong message pop uo
-  void wrongEmailMessage() {
+  void signUserInWithGoogle() async {
     showDialog(
         context: context,
-        builder: (context) {
-          return const AlertDialog(
-            title: Text('Wrong email'),
-          );
-        });
-  }
-
-  void wrongPasswordMessage() {
-    showDialog(
-        context: context,
-        builder: (context) {
-          return const AlertDialog(
-            title: Text('Wrong password'),
-          );
-        });
+        builder: (context) => const Center(
+              child: CircularProgressIndicator(
+                color: CustomColor.white,
+              ),
+            ));
+    try {
+      await AuthService().signInWithGoogle(context);
+      Navigator.pop(context);
+    } on FirebaseAuthException catch (e) {
+      Navigator.pop(context);
+      showErrorDialog(context, e.message!);
+    } catch (e) {
+      showErrorDialog(context, 'please check your email and password');
+    }
   }
 }
