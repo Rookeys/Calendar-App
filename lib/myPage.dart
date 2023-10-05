321b46a99309b48422e390a9032124bc327d3a2c
KoJaem <woalswhwh@naver.com>
2023-10-05 13:42:03 +0900

    style : myPage Profile UI 구현 완료 #5

--- lib/myPage.dart
+++ lib/myPage.dart
@@ -7,67 +7,81 @@ class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              decoration: const BoxDecoration(
-                color: CustomColor.skyBlue,
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(48),
-                  bottomRight: Radius.circular(48),
-                ),
-              ),
-              height: 200,
-              child: const Row(
-                children: [
-                  ClipRRect(
-                    clipBehavior: Clip.hardEdge,
-                    // Todo 이미지 사이즈 조정
-                    // borderRadius: BorderRadius.circular(48),
-                    child: Image(
-                      image: AssetImage('assets/example_profile.jpg'),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    color: CustomColor.skyBlue,
+                    borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(48),
+                      bottomRight: Radius.circular(48),
                     ),
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Name: KoJaem",
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: CustomColor.white,
-                          fontWeight: FontWeight.bold,
-                        ),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 4, // 그림자가 얼마나 드리울지 정함
+                        offset: const Offset(0, 4), // 그림자의 위치
+                        color: CustomColor.darkGray.withOpacity(0.5),
+                      )
+                    ]),
+                padding: const EdgeInsets.all(10),
+                height: 200,
+                child: Row(
+                  children: [
+                    ClipRRect(
+                      clipBehavior: Clip.hardEdge,
+                      borderRadius: BorderRadius.circular(100),
+                      child: const Image(
+                        image: AssetImage('assets/example_profile.jpg'),
+                        width: 130,
+                        height: 130,
+                        fit: BoxFit.cover,
                       ),
-                      Text(
-                        'Position: Front-End',
-                        style: TextStyle(
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    const Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Name: KoJaem",
+                          style: TextStyle(
                             fontSize: 16,
                             color: CustomColor.white,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        'Joined Team : 2023-03-13',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: CustomColor.white,
-                          fontWeight: FontWeight.bold,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                    ],
-                  )
-                ],
+                        Text(
+                          'Position: Front-End',
+                          style: TextStyle(
+                              fontSize: 16,
+                              color: CustomColor.white,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          'Joined Team : 2023-03-13',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: CustomColor.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
+              Container(
+                // color: CustomColor.green,
+                height: 200,
+              )
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
