be538db57c9ccbe5898f26adc28e996c094f33df
KoJaem <woalswhwh@naver.com>
2023-10-15 18:29:28 +0900

    feat : members page 라우팅 적용 및 UI 구현중 #5

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -1,5 +1,6 @@
 import 'package:calendar_app/calendar.dart';
 import 'package:calendar_app/home.dart';
+import 'package:calendar_app/members.dart';
 import 'package:calendar_app/myPage.dart';
 import 'package:calendar_app/myPageEdit.dart';
 import 'package:flutter/material.dart';
@@ -33,5 +34,9 @@ final GoRouter routes = GoRouter(routes: [
   GoRoute(
     path: '/myPage/edit',
     builder: (context, state) => const MyPageEdit(),
+  ),
+  GoRoute(
+    path: '/members',
+    builder: (context, state) => const Members(),
   )
 ]);
---
+++ lib/members.dart
@@ -0,0 +1,228 @@
+import 'package:calendar_app/constants/customColor.dart';
+import 'package:flutter/material.dart';
+
+var exampleJson = [
+  {
+    'date': '2023/09/08',
+    'status': 'Pending',
+    'reason': null,
+  },
+  {
+    'date': '2023/08/18',
+    'status': 'Done',
+    'reason': null,
+  },
+  {
+    'date': '2023/08/04',
+    'status': 'Reject',
+    'reason': '사유 : 회사 방송촬영 일정이 잡힘',
+  },
+  {
+    'date': '2023/09/08',
+    'status': 'Pending',
+    'reason': null,
+  },
+  {
+    'date': '2023/08/18',
+    'status': 'Done',
+    'reason': null,
+  },
+  {
+    'date': '2023/08/04',
+    'status': 'Reject',
+    'reason': '사유 : 회사 방송촬영 일정이 잡힘',
+  },
+  {
+    'date': '2023/09/08',
+    'status': 'Pending',
+    'reason': null,
+  },
+  {
+    'date': '2023/08/18',
+    'status': 'Done',
+    'reason': null,
+  },
+  {
+    'date': '2023/08/04',
+    'status': 'Reject',
+    'reason': '사유 : 회사 방송촬영 일정이 잡힘',
+  },
+  {
+    'date': '2023/09/08',
+    'status': 'Pending',
+    'reason': null,
+  },
+  {
+    'date': '2023/08/18',
+    'status': 'Done',
+    'reason': null,
+  },
+  {
+    'date': '2023/08/04',
+    'status': 'Reject',
+    'reason': '사유 : 회사 방송촬영 일정이 잡힘',
+  },
+  {
+    'date': '2023/09/08',
+    'status': 'Pending',
+    'reason': null,
+  },
+  {
+    'date': '2023/08/18',
+    'status': 'Done',
+    'reason': null,
+  },
+  {
+    'date': '2023/08/04',
+    'status': 'Reject',
+    'reason': '사유 : 회사 방송촬영 일정이 잡힘',
+  },
+  {
+    'date': '2023/09/08',
+    'status': 'Pending',
+    'reason': null,
+  },
+  {
+    'date': '2023/08/18',
+    'status': 'Done',
+    'reason': null,
+  },
+  {
+    'date': '2023/08/04',
+    'status': 'Reject',
+    'reason': '사유 : 회사 방송촬영 일정이 잡힘',
+  },
+];
+
+class Members extends StatelessWidget {
+  const Members({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: CustomColor.whiteBlue,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: ListView.separated(
+            separatorBuilder: (context, index) {
+              return const Padding(
+                padding: EdgeInsets.symmetric(vertical: 8),
+              );
+            },
+            // shrinkWrap: true,
+            // primary: false,
+            itemCount: exampleJson.length,
+            itemBuilder: (context, index) {
+              if (exampleJson[index]["status"] == 'Reject') {
+                return InfoBoxWithReason(
+                  date: exampleJson[index]['date'] as String,
+                  status: exampleJson[index]['status'] as String,
+                  reason: exampleJson[index]['reason'],
+                );
+              }
+              return InfoBox(
+                date: exampleJson[index]['date'] as String,
+                status: exampleJson[index]['status'] as String,
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class InfoBox extends StatelessWidget {
+  const InfoBox({
+    super.key,
+    required this.date,
+    required this.status,
+  });
+
+  final String date;
+  final String status;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 360,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        color:
+            status == 'Pending' ? CustomColor.lightYellow : CustomColor.skyBlue,
+      ),
+      child: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 70,
+            alignment: Alignment.centerLeft,
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+              '$date - $status',
+              style: const TextStyle(
+                fontSize: 16,
+                color: CustomColor.black,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class InfoBoxWithReason extends StatefulWidget {
+  const InfoBoxWithReason({
+    super.key,
+    required this.date,
+    required this.status,
+    this.reason,
+  });
+
+  final String date;
+  final String status;
+  final String? reason;
+
+  @override
+  State<InfoBoxWithReason> createState() => _InfoBoxWithReasonState();
+}
+
+class _InfoBoxWithReasonState extends State<InfoBoxWithReason> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 360,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        color: CustomColor.darkGray,
+      ),
+      child: Column(
+        children: [
+          Container(
+            height: 70,
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '${widget.date} - ${widget.status}',
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: CustomColor.white,
+                  ),
+                ),
+                const Icon(
+                  Icons.keyboard_arrow_down_outlined,
+                  color: CustomColor.white,
+                )
+              ],
+            ),
+          ),
+          // Reason Component
+        ],
+      ),
+    );
+  }
+}
