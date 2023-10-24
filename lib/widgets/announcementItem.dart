5431e9bb2fc853b4d565d04974c913a966b338b9
froggy1014 <clcl6084@gmail.com>
2023-10-25 01:01:02 +0900

    feat: apply modal on announcementItem widget  #7

--- lib/widgets/announcementItem.dart
+++ lib/widgets/announcementItem.dart
@@ -1,6 +1,7 @@
 import 'package:calendar_app/constants/categories.dart';
 import 'package:calendar_app/constants/customColor.dart';
 import 'package:calendar_app/models/announcement.dart';
+import 'package:calendar_app/widgets/readonlyModal.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -12,68 +13,83 @@ class AnnouncementListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(bottom: 10),
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-      decoration: BoxDecoration(
-        color: CustomColor.white,
-        borderRadius: BorderRadius.circular(10),
-        border: Border.all(
-          color: CustomColor.skyBlue,
-          width: 2,
+    return GestureDetector(
+      onTap: () {
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return ReadOnlyModal(
+                title: announcement.title,
+                userName: announcement.userName,
+                content: announcement.content,
+                category: announcement.category,
+              );
+            });
+      },
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 10),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        decoration: BoxDecoration(
+          color: CustomColor.white,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            color: CustomColor.skyBlue,
+            width: 2,
+          ),
         ),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(children: [
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 2),
-              decoration: BoxDecoration(
-                border: Border.all(color: CustomColor.skyBlue, width: 1),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Text(
-                categories[announcement.category] ?? '공지',
-                style: const TextStyle(
-                  fontSize: 10,
-                  fontWeight: FontWeight.bold,
-                  color: CustomColor.skyBlue,
-                  decoration: TextDecoration.none,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(children: [
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 2),
+                decoration: BoxDecoration(
+                  border: Border.all(color: CustomColor.skyBlue, width: 1),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Text(
+                  categories[announcement.category] ?? '공지',
+                  style: const TextStyle(
+                    fontSize: 10,
+                    fontWeight: FontWeight.bold,
+                    color: CustomColor.skyBlue,
+                    decoration: TextDecoration.none,
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(width: 10),
-            Expanded(
-              child: Text(
-                announcement.title,
-                overflow: TextOverflow.ellipsis,
-                style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: CustomColor.black,
-                    decoration: TextDecoration.none,
-                    overflow: TextOverflow.ellipsis),
-                softWrap: false,
-                maxLines: 1,
-              ),
-            )
-          ]),
-          const SizedBox(height: 5),
-          Row(
-            children: [
-              Text(
-                DateFormat('yyyy-MM-dd').format(announcement.createdDate),
-                style: const TextStyle(
-                  fontSize: 12,
-                  fontWeight: FontWeight.bold,
-                  color: CustomColor.darkGray,
-                  decoration: TextDecoration.none,
+              const SizedBox(width: 10),
+              Expanded(
+                child: Text(
+                  announcement.title,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: CustomColor.black,
+                      decoration: TextDecoration.none,
+                      overflow: TextOverflow.ellipsis),
+                  softWrap: false,
+                  maxLines: 1,
                 ),
               )
-            ],
-          )
-        ],
+            ]),
+            const SizedBox(height: 5),
+            Row(
+              children: [
+                Text(
+                  DateFormat('yyyy-MM-dd').format(announcement.createdDate),
+                  style: const TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                    color: CustomColor.darkGray,
+                    decoration: TextDecoration.none,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
