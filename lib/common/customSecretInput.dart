bffc54a7c05676db6f29fba8976e30a67819ff7c
Evan Lee <froggy@Evans-MacBook-Air.local>
2023-12-19 23:24:51 +0900

    feat: 공용 인풋 컴포넌트 색상 및 컨트롤러 프롭스 추가 #18

--- lib/common/customSecretInput.dart
+++ lib/common/customSecretInput.dart
@@ -7,11 +7,21 @@ class CustomSecretInput extends StatefulWidget {
     required this.label,
     required this.onSaved,
     required this.validator,
+    this.labelStyle,
+    this.style,
+    this.cursorColor,
+    this.inputContainerDecoration,
+    this.inputDecoration,
   });
 
   final String label;
   final FormFieldSetter onSaved;
   final FormFieldValidator validator;
+  final TextStyle? labelStyle;
+  final TextStyle? style;
+  final Color? cursorColor;
+  final BoxDecoration? inputContainerDecoration;
+  final InputDecoration? inputDecoration;
 
   @override
   State<CustomSecretInput> createState() => _CustomSecretInputState();
@@ -37,18 +47,25 @@ class _CustomSecretInputState extends State<CustomSecretInput> {
               fontSize: 20,
               fontWeight: FontWeight.bold,
               color: CustomColor.darkGray,
-            ),
+            ).merge(widget.labelStyle),
           ),
           TextFormField(
             obscureText: _secret,
             onSaved: widget.onSaved,
             validator: widget.validator,
-            // autovalidateMode: AutovalidateMode.always, // 제어컴포넌트
-            decoration: InputDecoration(
+            style: widget.style,
+            cursorColor: widget.cursorColor,
+            decoration: widget.inputDecoration?.copyWith(
               suffixIcon: GestureDetector(
                 child: _secret
-                    ? const Icon(Icons.visibility_outlined)
-                    : const Icon(Icons.visibility_off_outlined),
+                    ? Icon(
+                        Icons.visibility_outlined,
+                        color: widget.inputDecoration?.iconColor,
+                      )
+                    : Icon(
+                        Icons.visibility_off_outlined,
+                        color: widget.inputDecoration?.iconColor,
+                      ),
                 onTap: () => setState(() {
                   _secret = !_secret;
                 }),
