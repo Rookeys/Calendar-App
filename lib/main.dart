2c0c3eae91b542746cf240f7d90a88f883f30f80
Evan Lee <clcl6084@gmail.com>
2024-01-11 01:03:29 +0900

    feat: 스플레쉬 스크린 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,31 @@
 import 'package:calendar_app/config/routes.dart';
+import 'package:calendar_app/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 // import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
-  // 달력 언어 한글로 지원할거면 필요
-  // await initializeDateFormatting();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    // name: "rookeys-calendar",
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(const SplashScreenApp());
+}
+class SplashScreenApp extends StatelessWidget {
+  const SplashScreenApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SplashScreen(),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
