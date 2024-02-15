1bbec29e1bf909664635c7071de6ec036f0ffa0e
KoJaem <woalswhwh@naver.com>
2024-02-16 00:40:20 +0900

    feat: image picker 구현중 #17

--- lib/myPageEdit.dart
+++ lib/myPageEdit.dart
@@ -2,18 +2,27 @@ import 'package:calendar_app/constants/customColor.dart';
 import 'package:calendar_app/ImagePickerExample.dart';
 import 'package:calendar_app/widgets/myPageEdit/myPageEditForm.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class MyPageEdit extends StatelessWidget {
+class MyPageEdit extends StatefulWidget {
   const MyPageEdit({super.key});
 
   @override
+  State<MyPageEdit> createState() => _MyPageEditState();
+}
+
+class _MyPageEditState extends State<MyPageEdit> {
+  @override
+  void setState(VoidCallback fn) {
+    super.setState(fn);
+  }
+
+  XFile? file;
+
+  @override
   Widget build(BuildContext context) {
+    print('file $file');
     return Scaffold(
-      appBar: AppBar(
-        foregroundColor: CustomColor.black,
-        backgroundColor: CustomColor.whiteBlue,
-        toolbarHeight: 40,
-      ),
       backgroundColor: CustomColor.whiteBlue,
       body: SafeArea(
         child: SingleChildScrollView(
@@ -23,36 +32,41 @@ class MyPageEdit extends StatelessWidget {
                 const SizedBox(
                   height: 130,
                 ),
-                Stack(
-                  children: [
-                    ClipRRect(
-                      clipBehavior: Clip.hardEdge,
-                      borderRadius: BorderRadius.circular(100),
-                      child: const Image(
-                        image: AssetImage('assets/example_profile.jpg'),
-                        width: 180,
-                        height: 180,
-                        fit: BoxFit.cover,
+                GestureDetector(
+                  onTap: () => _pickImage(setState, file),
+                  child: Stack(
+                    children: [
+                      ClipRRect(
+                        clipBehavior: Clip.hardEdge,
+                        borderRadius: BorderRadius.circular(100),
+                        child: file != null
+                            ? const Text('test')
+                            : const Image(
+                                image: AssetImage('assets/example_profile.jpg'),
+                                width: 180,
+                                height: 180,
+                                fit: BoxFit.cover,
+                              ),
                       ),
-                    ),
-                    Positioned(
-                      top: 0,
-                      right: 10,
-                      child: GestureDetector(
-                        onTap: () => print("사진 가져오기 or 카메라"),
-                        child: ClipRRect(
-                          clipBehavior: Clip.hardEdge,
-                          borderRadius: BorderRadius.circular(100),
-                          child: const Image(
-                            image: AssetImage('assets/example_profile.jpg'),
-                            width: 40,
-                            height: 40,
-                            fit: BoxFit.cover,
+                      Positioned(
+                        top: 0,
+                        right: 10,
+                        child: GestureDetector(
+                          onTap: () => print("사진 가져오기 or 카메라"),
+                          child: ClipRRect(
+                            clipBehavior: Clip.hardEdge,
+                            borderRadius: BorderRadius.circular(100),
+                            child: const Image(
+                              image: AssetImage('assets/example_profile.jpg'),
+                              width: 40,
+                              height: 40,
+                              fit: BoxFit.cover,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
                 const SizedBox(height: 50),
                 const MyPageEditForm(),
@@ -63,5 +77,15 @@ class MyPageEdit extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+Future<void> _pickImage(Function setState, XFile? file) async {
+  final XFile? pickedFile =
+      await ImagePicker().pickImage(source: ImageSource.gallery);
+  if (pickedFile != null) {
+    setState(() {
+      file = XFile(pickedFile.path);
+    });
   }
 }
